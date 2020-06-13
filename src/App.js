00213cdb15d511f6b63705b6208fb35c331dbfe1
aribfp <aribpelayo@gmail.com>
2020-06-14 03:52:03 +1200

    added initial content

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './nfap.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} alt="logo" />
+        <br/> <br/> <br/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          NO FAAAAAAAAAAAAAAAAAAP!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
